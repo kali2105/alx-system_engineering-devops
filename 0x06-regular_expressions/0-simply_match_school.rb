351e5dc79aac7e045b0d946db0cfa24a52f332b4
Anass El Hamdi Alaoui <anassalaoui0593@gmail.com>
2024-01-04 13:20:01 -0800

    The first 2 mandatory task on ruby

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# Ruby regex that matching School
+
+puts ARGV[0].scan(/School/).join
---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# Ruby Repetition Token
+
+puts ARGV[0].scan(/hbt{2,5}n/).join
